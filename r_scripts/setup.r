ede8d8892d735c6139a5bea9d83ceb872e92aa77
Chris Smith <chrisroadmap@gmail.com>
2022-12-21 18:13:14 +0000

    finish sampling ebm3 stochastic for calibration v1.0

--- r_scripts/setup.r
+++ r_scripts/setup.r
@@ -1,5 +1,5 @@
 # Run with R 4.1.1 on Windows 10
 
 install.packages("devtools")
-devtools::install_github("donaldcummins/EBM")
+devtools::install_github("chrisroadmap/EBM@optimizer-bounds")
 library(EBM)
