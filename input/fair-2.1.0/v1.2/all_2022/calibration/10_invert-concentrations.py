f71dee4b46babc8b58e4033cbe19da8fb9dd58a3
Chris Smith <chrisroadmap@gmail.com>
2023-03-21 09:02:15 +0000

    WIP: machine transfer

--- input/fair-2.1.0/v1.2/all_2022/calibration/10_invert-concentrations.py
+++ input/fair-2.1.0/v1.2/all_2022/calibration/10_invert-concentrations.py
@@ -94,7 +94,7 @@ properties = {
         'greenhouse_gas': True,
         'aerosol_chemistry_from_emissions': False,
         'aerosol_chemistry_from_concentration': False,
-    } for specie in species    
+    } for specie in species
 }
 
 f.define_species(species, properties)
@@ -165,118 +165,34 @@ for specie in species:
 
 print(f.concentration[0, ...])
 
+
 # default AR6 lifetime etc
+
+# TODO: calculate steady state pre-industrial lifetime for each GHG
 f.fill_species_configs()
+for specie in species:
+    #fill(f.species_configs['baseline_concentration'], f.concentration.loc[dict(timebounds=1750, specie=specie, scenario='historical', config='historical')], specie=specie)
+    fill(f.species_configs['baseline_concentration'], 0, specie=specie)
+    fill(f.species_configs['baseline_emissions'], 0, specie=specie)
 fill(f.temperature, 0)
 f.run(progress=1-progress)
 
 print(f.emissions[0, ...])
 
-import sys
-sys.exit()
-
-input_obs = {}
-input_obs['SF6'] = df_conc_obs['SF6'].values
-
-x = np.arange(1750.5, 2023)
-y = df_conc_obs['SF6'].values
-f = interp1d(x, y)
-input_obs['SF6'][1:] = f(np.arange(1751, 2023))
-input_obs['SF6'][0] = 0
-
-df_emis_obs = pd.read_csv(f'../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/emissions/primap_ceds_gfed_1750-2022.csv')
-emis_obs = df_emis_obs.loc[df_emis_obs['Variable']=='Emissions|SF6', '1750':'2022'].values.squeeze()
-
-# baselines are 1850! so "base" lifetime out is for 1750!
-baseline_obs = {}
-for species in ["SF6"]:
-    baseline_obs[species] = input_obs[species][100]
-
-burden_per_emission = 1 / (5.1352e18 / 1e18 * 146.06 / 28.97)
-partition_fraction = 1
-pre_industrial_concentration = 0#729.2
-natural_emissions_adjustment = 0#emis_ch4[0]
-
-def fit_precursors(x, rbase):
-    conc_sf6 = np.zeros(273)
-    gas_boxes = 0
-    airborne_emissions = 0
-
-    for i in range(273):
-        conc_sf6[i], gas_boxes, airborne_emissions = one_box(
-            emis_obs[i],
-            gas_boxes,
-            airborne_emissions,
-            burden_per_emission,
-            rbase,
-            1,#lifetime_scaling[i],
-            partition_fraction,
-            pre_industrial_concentration=0,
-            timestep=1,
-            natural_emissions_adjustment=0#natural_emissions_adjustment,
-        )
-    return conc_sf6
-
-
-# natural bounds from global methane budget (part of GCP)
-p, cov = scipy.optimize.curve_fit(
-    fit_precursors,
-    emis_obs,
-    input_obs["SF6"],
+df_out = pd.DataFrame(
+    f.emissions[:, 0, 0, :],
+    index=np.arange(1750.5, 2022),
+    columns=species
 )
 
-parameters = {}
-
-parameters["best_fit"] = {
-    "base": p[0],
-}
-
-# these are the feedback values per ppb / per Mt that go into FaIR
-print(parameters["best_fit"])
-
-conc_sf6 = np.zeros(273)
-gas_boxes = 0
-airborne_emissions = 0
-
-for i in range(273):
-    conc_sf6[i], gas_boxes, airborne_emissions = one_box(
-        emis_obs[i],
-        gas_boxes,
-        airborne_emissions,
-        burden_per_emission,
-        parameters["best_fit"]["base"],
-        1,#lifetime_scaling["best_fit"][i],
-        partition_fraction,
-        pre_industrial_concentration=0,
-        timestep=1,
-        natural_emissions_adjustment=0#natural_emissions_adjustment,
-    )
-
-if plots:
-    fig, ax = pl.subplots(1, 1, figsize=(3.5, 3.5))
-
-    ax.plot(
-        np.arange(1930, 2023), conc_sf6[180:], color="0.5", label="Best fit"
-    )
-    ax.plot(
-        np.arange(1930, 2023), input_obs["SF6"][180:], color="k", label="observations"
-    )
-    ax.set_ylabel("ppt")
-    ax.set_xlim(1930, 2023)
-    ax.legend(frameon=False)
-    ax.set_title("SF$_6$ concentration")
-
-    fig.tight_layout()
-    pl.savefig(
-        f"../../../../../plots/fair-{fair_v}/v{cal_v}/{constraint_set}/"
-        "sf6_calibrations.png"
-    )
-    pl.savefig(
-        f"../../../../../plots/fair-{fair_v}/v{cal_v}/{constraint_set}/"
-        "sf6_calibrations.pdf"
-    )
-    pl.close()
-
+os.makedirs(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/emissions/",
+    exist_ok=True
+)
+df_out.to_csv(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/emissions/"
+    "minor_ghg_inverse_1750-2021.csv"
+)
 # # these are the feedback values that go into FaIR
 # out = np.empty((1, 1))
 # out[0, 0] = parameters["best_fit"]["base"]
@@ -286,10 +202,7 @@ if plots:
 #     columns=["base"],
 #     index=["historical_best"],
 # )
-# os.makedirs(
-#     f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/calibrations/",
-#     exist_ok=True
-# )
+
 # df.to_csv(
 #     f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/calibrations/"
 #     "SF6_lifetime.csv"
