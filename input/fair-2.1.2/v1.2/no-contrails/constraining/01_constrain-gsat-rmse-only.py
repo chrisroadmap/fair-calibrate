5920d433f266f975ecbba153bdf407a006304583
Chris Smith <chrisroadmap@gmail.com>
2023-11-21 17:32:06 +0000

    no-contrails

---
+++ input/fair-2.1.2/v1.2/no-contrails/constraining/01_constrain-gsat-rmse-only.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""First constraint: RMSE < 0.17 K"""
+
+import os
+
+import matplotlib.pyplot as pl
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+from fair import __version__
+from tqdm.auto import tqdm
+
+pl.switch_backend('agg')
+
+load_dotenv()
+pl.style.use("../../../../../defaults.mplstyle")
+
+print("Doing RMSE constraint...")
+
+cal_v = os.getenv("CALIBRATION_VERSION")
+fair_v = os.getenv("FAIR_VERSION")
+constraint_set = os.getenv("CONSTRAINT_SET")
+samples = int(os.getenv("PRIOR_SAMPLES"))
+plots = os.getenv("PLOTS", "False").lower() in ("true", "1", "t")
+progress = os.getenv("PROGRESS", "False").lower() in ("true", "1", "t")
+
+assert fair_v == __version__
+
+
+temp_in = np.load(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/prior_runs/"
+    "temperature_1850-2101.npy"
+)
+
+df_gmst = pd.read_csv("../../../../../data/forcing/IGCC_GMST_1850-2022.csv")
+gmst = df_gmst["gmst"].values
+
+
+def rmse(obs, mod):
+    return np.sqrt(np.sum((obs - mod) ** 2) / len(obs))
+
+
+weights = np.ones(52)
+weights[0] = 0.5
+weights[-1] = 0.5
+
+rmse_temp = np.zeros((samples))
+
+if plots:
+    fig, ax = pl.subplots(figsize=(5, 5))
+    ax.fill_between(
+        np.arange(1850, 2102),
+        np.min(temp_in - np.average(temp_in[:52, :], weights=weights, axis=0), axis=1),
+        np.max(temp_in - np.average(temp_in[:52, :], weights=weights, axis=0), axis=1),
+        color="#000000",
+        alpha=0.2,
+    )
+    ax.fill_between(
+        np.arange(1850, 2102),
+        np.percentile(
+            temp_in - np.average(temp_in[:52, :], weights=weights, axis=0), 5, axis=1
+        ),
+        np.percentile(
+            temp_in - np.average(temp_in[:52, :], weights=weights, axis=0), 95, axis=1
+        ),
+        color="#000000",
+        alpha=0.2,
+    )
+    ax.fill_between(
+        np.arange(1850, 2102),
+        np.percentile(
+            temp_in - np.average(temp_in[:52, :], weights=weights, axis=0), 16, axis=1
+        ),
+        np.percentile(
+            temp_in - np.average(temp_in[:52, :], weights=weights, axis=0), 84, axis=1
+        ),
+        color="#000000",
+        alpha=0.2,
+    )
+    ax.plot(
+        np.arange(1850, 2102),
+        np.median(
+            temp_in - np.average(temp_in[:52, :], weights=weights, axis=0), axis=1
+        ),
+        color="#000000",
+    )
+    ax.plot(np.arange(1850.5, 2023), gmst, color="b")
+
+    ax.set_xlim(1850, 2100)
+    ax.set_ylim(-1, 5)
+    ax.set_ylabel("°C relative to 1850-1900")
+    ax.axhline(0, color="k", ls=":", lw=0.5)
+    pl.title("Temperature anomaly: ssp245 prior")
+    pl.tight_layout()
+    os.makedirs(
+        f"../../../../../plots/fair-{fair_v}/v{cal_v}/{constraint_set}/", exist_ok=True
+    )
+    pl.savefig(
+        f"../../../../../plots/fair-{fair_v}/v{cal_v}/{constraint_set}/prior_ssp245.png"
+    )
+    pl.savefig(
+        f"../../../../../plots/fair-{fair_v}/v{cal_v}/{constraint_set}/prior_ssp245.pdf"
+    )
+    pl.close()
+
+# temperature is on timebounds, and observations are midyears
+# but, this is OK, since we are subtracting a consistent baseline (1850-1900, weighting the bounding
+# timebounds as 0.5)
+# e.g. 1993.0 timebound has big pinatubo hit, timebound 143
+# in obs this is 1992.5, timepoint 142
+# compare the timebound after the obs, since the forcing has had chance to affect both the obs timepoint
+# and the later timebound.
+# the goal of RMSE is as much to match the shape of warming as the magnitude; we do not want
+# to average out internal variability in the model or the obs.
+for i in tqdm(range(samples), disable=1 - progress):
+    rmse_temp[i] = rmse(
+        gmst[:173],
+        temp_in[1:174, i] - np.average(temp_in[:52, i], weights=weights, axis=0),
+    )
+
+accept_temp = rmse_temp < 0.17
+print("Passing RMSE constraint:", np.sum(accept_temp))
+valid_temp = np.arange(samples, dtype=int)[accept_temp]
+
+if plots:
+    fig, ax = pl.subplots(figsize=(5, 5))
+    ax.fill_between(
+        np.arange(1850, 2102),
+        np.min(
+            temp_in[:, accept_temp]
+            - np.average(temp_in[:52, accept_temp], weights=weights, axis=0),
+            axis=1,
+        ),
+        np.max(
+            temp_in[:, accept_temp]
+            - np.average(temp_in[:52, accept_temp], weights=weights, axis=0),
+            axis=1,
+        ),
+        color="#000000",
+        alpha=0.2,
+    )
+    ax.fill_between(
+        np.arange(1850.5, 2102),
+        np.percentile(
+            temp_in[:, accept_temp]
+            - np.average(temp_in[:52, accept_temp], weights=weights, axis=0),
+            5,
+            axis=1,
+        ),
+        np.percentile(
+            temp_in[:, accept_temp]
+            - np.average(temp_in[:52, accept_temp], weights=weights, axis=0),
+            95,
+            axis=1,
+        ),
+        color="#000000",
+        alpha=0.2,
+    )
+    ax.fill_between(
+        np.arange(1850.5, 2102),
+        np.percentile(
+            temp_in[:, accept_temp]
+            - np.average(temp_in[:52, accept_temp], weights=weights, axis=0),
+            16,
+            axis=1,
+        ),
+        np.percentile(
+            temp_in[:, accept_temp]
+            - np.average(temp_in[:52, accept_temp], weights=weights, axis=0),
+            84,
+            axis=1,
+        ),
+        color="#000000",
+        alpha=0.2,
+    )
+    ax.plot(
+        np.arange(1850.5, 2102),
+        np.median(
+            temp_in[:, accept_temp]
+            - np.average(temp_in[:52, accept_temp], weights=weights, axis=0),
+            axis=1,
+        ),
+        color="#000000",
+    )
+
+    ax.plot(np.arange(1850.5, 2023), gmst, color="b")
+
+    ax.set_xlim(1850, 2100)
+    ax.set_ylim(-1, 5)
+    ax.set_ylabel("°C relative to 1850-1900")
+    ax.axhline(0, color="k", ls=":", lw=0.5)
+    pl.title("Temperature anomaly: ssp245 RMSE constraint")
+    pl.tight_layout()
+    pl.savefig(
+        f"../../../../../plots/fair-{fair_v}/v{cal_v}/{constraint_set}/"
+        "post_rsme_ssp245.png"
+    )
+    pl.savefig(
+        f"../../../../../plots/fair-{fair_v}/v{cal_v}/{constraint_set}/"
+        "post_rsme_ssp245.pdf"
+    )
+    pl.close()
+
+valid_temp = np.arange(samples, dtype=int)[accept_temp]
+os.makedirs(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/posteriors",
+    exist_ok=True,
+)
+np.savetxt(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/posteriors/"
+    "runids_rmse_pass.csv",
+    valid_temp.astype(int),
+    fmt="%d",
+)
---
+++ input/fair-2.1.2/v1.2/no-contrails/sampling/09_make-ssp-emissions-binary.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Make emissions binary file."""
+#
+# SSPs from RCMIP
+
+import os
+
+import pandas as pd
+import pooch
+from dotenv import load_dotenv
+from fair import FAIR, __version__
+from fair.io import read_properties
+
+load_dotenv()
+
+
+cal_v = os.getenv("CALIBRATION_VERSION")
+fair_v = os.getenv("FAIR_VERSION")
+constraint_set = os.getenv("CONSTRAINT_SET")
+samples = int(os.getenv("PRIOR_SAMPLES"))
+
+assert fair_v == __version__
+
+print("Making SSP emissions binary...")
+
+scenarios = [
+    "ssp119",
+    "ssp126",
+    "ssp245",
+    "ssp370",
+    "ssp434",
+    "ssp460",
+    "ssp534-over",
+    "ssp585",
+]
+
+species, properties = read_properties()
+species.remove("NOx aviation")
+species.remove("Contrails")
+
+f = FAIR(ch4_method="thornhill2021")
+f.define_time(1750, 2500, 1)
+f.define_configs(["unspecified"])
+f.define_scenarios(scenarios)
+f.define_species(species, properties)
+
+f.allocate()
+f.fill_from_rcmip()
+
+# Fix NOx.
+rcmip_emissions_file = pooch.retrieve(
+    url="doi:10.5281/zenodo.4589756/rcmip-emissions-annual-means-v5-1-0.csv",
+    known_hash="md5:4044106f55ca65b094670e7577eaf9b3",
+)
+df_emis = pd.read_csv(rcmip_emissions_file)
+gfed_sectors = [
+    "Emissions|NOx|MAGICC AFOLU|Agricultural Waste Burning",
+    "Emissions|NOx|MAGICC AFOLU|Forest Burning",
+    "Emissions|NOx|MAGICC AFOLU|Grassland Burning",
+    "Emissions|NOx|MAGICC AFOLU|Peat Burning",
+]
+for scenario in scenarios:
+    f.emissions.loc[dict(specie="NOx", scenario=scenario, config="unspecified")] = (
+        df_emis.loc[
+            (df_emis["Scenario"] == scenario)
+            & (df_emis["Region"] == "World")
+            & (df_emis["Variable"].isin(gfed_sectors)),
+            "1750":"2500",
+        ]
+        .interpolate(axis=1)
+        .values.squeeze()
+        .sum(axis=0)
+        * 46.006
+        / 30.006
+        + df_emis.loc[
+            (df_emis["Scenario"] == scenario)
+            & (df_emis["Region"] == "World")
+            & (df_emis["Variable"] == "Emissions|NOx|MAGICC AFOLU|Agriculture"),
+            "1750":"2500",
+        ]
+        .interpolate(axis=1)
+        .values.squeeze()
+        + df_emis.loc[
+            (df_emis["Scenario"] == scenario)
+            & (df_emis["Region"] == "World")
+            & (df_emis["Variable"] == "Emissions|NOx|MAGICC Fossil and Industrial"),
+            "1750":"2500",
+        ]
+        .interpolate(axis=1)
+        .values.squeeze()
+    )[:750]
+
+os.makedirs(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/emissions/",
+    exist_ok=True,
+)
+
+f.emissions.to_netcdf(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/emissions/"
+    "ssp_emissions_1750-2500.nc"
+)
