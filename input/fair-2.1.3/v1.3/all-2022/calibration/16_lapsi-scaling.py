46e069806d1be4d94e8ffaae6a1d3915c04407dc
Chris Smith <chrisroadmap@gmail.com>
2023-12-08 18:28:53 +0000

    full updated emissions calibration

---
+++ input/fair-2.1.3/v1.3/all-2022/calibration/16_lapsi-scaling.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Determining the land use scale factor to use."""
+
+# Correlate cumulative CO2 AFOLU emissions to land use forcing in the present
+
+import os
+
+import numpy as np
+import pandas as pd
+import pooch
+import scipy.optimize
+import scipy.stats
+import xarray as xr
+from dotenv import load_dotenv
+from fair import __version__
+
+load_dotenv()
+
+print("Calculating land use scale factor...")
+
+cal_v = os.getenv("CALIBRATION_VERSION")
+fair_v = os.getenv("FAIR_VERSION")
+constraint_set = os.getenv("CONSTRAINT_SET")
+datadir = os.getenv("DATADIR")
+
+target_forcing = +0.08
+base_year = 1750
+assessment_year = 2019
+
+df_emis = pd.read_csv(f'../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/emissions/all_1750-2022.csv')
+bc = (
+    df_emis.loc[df_emis["Variable"]=="Emissions|BC", str(assessment_year)] -
+    df_emis.loc[df_emis["Variable"]=="Emissions|BC", str(base_year)]
+).values[0]
+
+lapsi_scale_factor = target_forcing / bc
+
+df = pd.DataFrame(
+    lapsi_scale_factor,
+    columns=["BC"],
+    index=["historical_best"],
+)
+os.makedirs(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/calibrations/",
+    exist_ok=True
+)
+df.to_csv(
+    f"../../../../../output/fair-{fair_v}/v{cal_v}/{constraint_set}/calibrations/"
+    "lapsi_scale_factor.csv"
+)
