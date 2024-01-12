b55a53b1285df52b0380fd369a55272e8964d39f
Chris Smith <chrisroadmap@gmail.com>
2024-01-12 15:31:30 +0000

    update sampling

--- input/fair-2.1.3/v1.4/AR6-updated_no-contrails/sampling/01_climate-response-sampling-ebm3.py
+++ input/fair-2.1.3/v1.4/AR6-updated_no-contrails/sampling/01_climate-response-sampling-ebm3.py
@@ -45,6 +45,16 @@ df = pd.read_csv(
 )
 models = df["model"].unique()
 
+# NorESM2-LM is currently INCLUDED. Comment below left in for train-of-thought.
+# Executive decision: remove NorESM2-LM. It is always incredibly difficult to calibrate.
+# Sometimes it fails completely, sometimes it gives nonsense values that wreck the rest
+# of the distribution.
+#
+# if "NorESM2-LM" in models:
+#     noresm2lm_index = np.argwhere(models=="NorESM2-LM")
+#     np.delete(models, noresm2lm_index)
+#     print("NorESM2-LM is being removed.")
+
 for model in models:
     print(model, df.loc[df["model"] == model, "run"].values)
 
@@ -98,7 +108,7 @@ params = pd.DataFrame(params)
 print(params.corr())
 
 if plots:
-    fig = pl.figure(figsize=(5.875, 5.875))
+    fig = pl.figure(figsize=(18 / 2.54, 13 / 2.54))
     pd.plotting.scatter_matrix(params)
     pl.suptitle("Distributions and correlations of CMIP6 calibrations")
     pl.tight_layout()
@@ -124,8 +134,8 @@ ebm_sample = kde.resample(size=int(samples * 4), seed=2181882)
 # remove unphysical combinations
 for col in range(10):
     ebm_sample[:, ebm_sample[col, :] <= 0] = np.nan
-ebm_sample[:, ebm_sample[0, :] <= 0.8] = np.nan  # gamma
-ebm_sample[:, ebm_sample[1, :] <= 2] = np.nan  # C1
+ebm_sample[:, ebm_sample[0, :] <= 0.5] = np.nan  # gamma
+ebm_sample[:, ebm_sample[1, :] <= 1.8] = np.nan  # C1
 ebm_sample[:, ebm_sample[2, :] <= ebm_sample[1, :]] = np.nan  # C2
 ebm_sample[:, ebm_sample[3, :] <= ebm_sample[2, :]] = np.nan  # C3
 ebm_sample[:, ebm_sample[4, :] <= 0.3] = np.nan  # kappa1 = lambda
