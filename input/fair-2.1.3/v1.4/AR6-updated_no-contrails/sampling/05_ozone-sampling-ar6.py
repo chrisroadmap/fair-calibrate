3476a1d84e99291a7fb8dbd41f1fe9e9940e6428
Chris Smith <chrisroadmap@gmail.com>
2024-01-12 16:35:49 +0000

    update ozone plot

--- input/fair-2.1.3/v1.4/AR6-updated_no-contrails/sampling/05_ozone-sampling-ar6.py
+++ input/fair-2.1.3/v1.4/AR6-updated_no-contrails/sampling/05_ozone-sampling-ar6.py
@@ -390,7 +390,7 @@ p, cov = curve_fit(
     fit_precursors,
     ts[:270, :].T - ts[0:1, :].T,
     o3total[:270] - o3total[0],
-    bounds=(  # assumed likely range from Thornhill - maybe could be wider?
+    bounds=(  # very likely range from Thornhill - maybe could be wider?
         (
             0.09 / delta_Cch4 / fac_cmip6_skeie,
             0.01 / delta_Cn2o / fac_cmip6_skeie,
@@ -420,10 +420,11 @@ forcing = (
 )
 
 if plots:
-    pl.plot(np.arange(1750.5, 2020), forcing, label="best estimate fit")
-    pl.plot(np.arange(1750.5, 2021), o3total, label="Skeie et al. 2020 mean")
+    pl.figure(figsize=(9 / 2.54, 9 / 2.54))
+    pl.plot(np.arange(1750.5, 2020), forcing, label="best estimate fit", color="0.5")
+    pl.plot(np.arange(1750.5, 2021), o3total, label="Skeie et al. 2020 mean", color="k")
     pl.legend()
-    pl.title("Ozone forcing calibration to CMIP6 mean")
+    pl.title("Ozone forcing (no feedbacks)")
     pl.ylabel("W m$^{-2}$")
     pl.xlim(1750, 2020)
     pl.tight_layout()
